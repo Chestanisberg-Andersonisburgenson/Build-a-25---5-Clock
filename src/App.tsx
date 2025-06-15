a62c2f8fed90928b88a3b390c480833c63504968
The <Groundwalkers@outlook.com>
2025-06-16 00:38:52 +0100

    Add initial layout for the clock application

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,18 @@ function App() {
 
   return (
     <>
-     
+      <p id="break-label"></p>
+      <p id="session-label"></p>
+<button id="break-decrement"></button>
+<button id="session-decrement"></button>
+<p id="break-length">5</p>
+<p id="session-length">25</p>
+<p id="time-left">25:00</p>
+<button id="start_stop"></button>
+<button id="reset"></button>
     </>
-  )
+
+)
 }
 
 export default App
