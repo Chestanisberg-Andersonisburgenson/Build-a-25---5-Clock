e72c912b9039f3aaf839358f0f051950abcfd16f
The <Groundwalkers@outlook.com>
2025-06-16 23:36:10 +0100

    Refactor App component structure and update styles for clock application

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,39 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <p id="break-label"></p>
-      <p id="session-label"></p>
-<button id="break-decrement"></button>
-<button id="session-decrement"></button>
-<p id="break-length">5</p>
-<p id="session-length">25</p>
-<p id="time-left">25:00</p>
-<button id="start_stop"></button>
-<button id="reset"></button>
-    </>
+    <div id="app">
+      <h1>25 + 5 Clock</h1>
+      
+      <div id="length-controls">
+        <div id="break-controls">
+          <p id="break-label">Break Length</p>
+          <div className="arrow-controls">
+            <button id="break-decrement">⬇</button>
+            <span id="break-length">5</span>
+            <button id="break-increment">⬆</button>
+          </div>
+        </div>
+
+        <div id="session-controls">
+          <p id="session-label">Session Length</p>
+          <div className="arrow-controls">
+            <button id="session-decrement">⬇</button>
+            <span id="session-length">25</span>
+            <button id="session-increment">⬆</button>
+          </div>
+        </div>          
+      </div>
+
+        <div id="time-box">
+          <p id="timer-label">Session</p>
+          <p id="time-left">25:00</p>
+        </div>        
+
+      <div id="controls">
+        <button id="start_stop">⏯</button>        
+        <button id="reset">🔁</button>
+      </div>
+    </div>
 
 )
 }
